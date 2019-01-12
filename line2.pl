ae7e74df5da073d8bff9d90e30e364dee83799a3
Paolo Jovon <paolo.jovon@gmail.com>
2019-01-12 11:54:33 +0000

    Fixed Perl script

--- line2.pl
+++ line2.pl
@@ -1,3 +1,3 @@
 #!/usr/bin/env perl
-print "He was happy, but deep inside he felt like something was wrong.";
-run "python", "line3.py";
+print "He was happy, but deep inside he felt like something was wrong.\n";
+system("python", "line3.py");
